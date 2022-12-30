88164097309bd170af66e3cbbf6ef51187ca2f6c
the-geniustech <holumidey70@gmail.com>
2022-12-30 13:23:38 +0100

    'Change2'

--- home-page-script.js
+++ home-page-script.js
@@ -177,7 +177,7 @@ const loadImg = function (entries, observer) {
   if (!entry.isIntersecting) return;
 
   // Replace src with data-src
-  entry.target.src = entry.target.dataset.src;
+  // entry.target.src = entry.target.dataset.src;
 
   entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
